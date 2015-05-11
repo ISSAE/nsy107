9ed0e261f91fe939406e824542f6d177928f8b31
Pascal E Fares <pfares@cofares.net>
2015-05-11 17:35:09 +0300

    Echo Server pour les auditeurs NSY107 Cnam Liban

---
+++ Sockets/Echo/EchoServer/src/echoserver/EchoServer.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package echoserver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author pascalfares
+ */
+public class EchoServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Usage: java EchoServer <port number>");
+            System.exit(1);
+        }
+
+        int portNumber = Integer.parseInt(args[0]);
+
+        try (ServerSocket serverSocket
+                = new ServerSocket(Integer.parseInt(args[0]));
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter out
+                = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(clientSocket.getInputStream()));) {
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                out.println(inputLine);
+            }
+        } catch (IOException e) {
+            System.out.println("Exception en ecoutant le port "
+                    + portNumber + " ou en ecoutant la connection");
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
