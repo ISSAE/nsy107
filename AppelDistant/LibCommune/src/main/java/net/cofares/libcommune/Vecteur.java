c124c4facd6c8bd0b5bf6a1584e59eb9012624a2
Pascal E Fares <pfares@cofares.net>
2015-05-18 20:57:43 +0300

    LibVecteur maven

---
+++ AppelDistant/LibCommune/src/main/java/net/cofares/libcommune/Vecteur.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2015 pascalfares
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package net.cofares.libcommune;
+
+/**
+ *
+ * @author pascalfares
+ */
+public class Vecteur {
+    
+    private int x;
+
+    private int y;
+
+    /**
+     * Get the value of y
+     *
+     * @return the value of y
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * Set the value of y
+     *
+     * @param y new value of y
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    /**
+     * Get the value of x
+     *
+     * @return the value of x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * Set the value of x
+     *
+     * @param x new value of x
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    
+}
