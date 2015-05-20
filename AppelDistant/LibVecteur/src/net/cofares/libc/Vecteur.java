05bb4177c997bc794267ecfc0c8e9866bdd68b59
Pascal E Fares <pfares@cofares.net>
2015-05-20 13:02:25 +0300

    Préparation marshaling unmarshaling

--- AppelDistant/LibVecteur/src/net/cofares/libc/Vecteur.java
+++ AppelDistant/LibVecteur/src/net/cofares/libc/Vecteur.java
@@ -6,7 +6,12 @@
 
 package net.cofares.libc;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.Serializable;
+import java.io.StreamTokenizer;
+import java.io.StringReader;
+import java.io.StringWriter;
 
 /**
  *
@@ -57,5 +62,43 @@ public class Vecteur implements Serializable {
     @Override
     public String toString() {
         return "("+ getX() + "," +getY() +  ")";
+    }
+    
+    public String marshaling() {
+        StringWriter sb = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(sb);
+        printWriter.printf("{%d,%d}", getX(), getY());      
+        return sb.toString();
+    }
+    
+    /**
+     * Vecteur -> {Entier,Entier}
+     * Entier -> Nombre entoer java
+     * @param data
+     * @return 
+     * @throws java.io.IOException 
+     */
+    public static Vecteur unmarshal(String data) throws IOException{
+        Vecteur rep = new Vecteur();
+        StreamTokenizer s = new StreamTokenizer (new StringReader(data));
+        s.nextToken();
+        //On doit avoir {
+        if (s.ttype != '{') return null;
+        s.nextToken(); //skip { prepare le prohain token
+        //on doit avoir un int
+        if (s.ttype == StreamTokenizer.TT_NUMBER) {
+            rep.setX((int) s.nval);
+            s.nextToken();
+        } else return null;
+        if (s.ttype != ',') return null;
+        s.nextToken();
+        if (s.ttype == StreamTokenizer.TT_NUMBER) {
+            rep.setY((int) s.nval);
+            s.nextToken();
+        } else return null;
+        //On doit terminer par }
+        if (s.ttype != '}') return null;
+        
+        return rep;
     }
 }
